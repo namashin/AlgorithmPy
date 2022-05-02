77106d52468e1e8bc05b1dd108a352b583745cee
namashin <65502681+namashin@users.noreply.github.com>
2022-05-02 23:25:04 +0900

    Update main.py

--- Question/main.py
+++ Question/main.py
@@ -3,6 +3,38 @@ from typing import List, Tuple
 from collections import Counter
 
 
+# 電話番号のメモニック
+NUM_ALPHABET = {
+    0: '+',
+    1: '@',
+    2: 'ABC',
+    3: 'DEF',
+    4: 'GHI',
+    5: 'JKL',
+    6: 'MNO',
+    7: 'PQRS',
+    8: 'TUV',
+    9: 'WXYZ',
+}
+
+
+def phone_mnemonic(telephone_number: str):
+    phone_number = [int(s) for s in telephone_number.replace('-', '')]
+
+    candidate = []
+    tmp = [''] * len(phone_number)
+
+    def find_candidate_alphabet(digit: int = 0) -> None:
+        if digit == len(phone_number):
+            candidate.append(''.join(tmp))
+        else:
+            for char in NUM_ALPHABET[phone_number[digit]]:
+                tmp[digit] = char
+                find_candidate_alphabet(digit+1)
+
+    find_candidate_alphabet()
+    return candidate
+
 
 # 素数判定
 def is_prime(number: int) -> bool:
